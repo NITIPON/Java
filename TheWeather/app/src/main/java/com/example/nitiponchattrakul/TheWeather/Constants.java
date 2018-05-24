66ab29c400f023c67305d9307da11aafe335f9d3
Nitipon Chattrakul <nitipon056@hotmail.com>
2018-05-24 19:40:43 +0700

    first

---
+++ TheWeather/app/src/main/java/com/example/nitiponchattrakul/TheWeather/Constants.java
@@ -0,0 +1,7 @@
+package com.example.nitiponchattrakul.TheWeather;
+
+public class Constants {
+    public static final String  DEFAULT_CITY = "Thailand";
+    public static final String  DEFAULT_LAT = "13.7627";
+    public static final String  DEFAULT_LON = "100.5408565";
+}
---
+++ TheWeather/app/src/main/java/com/example/nitiponchattrakul/TheWeather/tasks/ParseResult.java
@@ -0,0 +1,3 @@
+package com.example.nitiponchattrakul.TheWeather.tasks;
+
+public enum ParseResult {OK, JSON_EXCEPTION, CITY_NOT_FOUND}
